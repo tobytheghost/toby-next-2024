c84734f73e06ece8d6297bed01b4825e429244a3
Toby <tobytheghost@hotmail.com>
2023-12-18 10:13:55 +0000

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "~/styles/globals.css";
+
+import { Poppins } from "next/font/google";
+
+const inter = Poppins({
+  weight: ["400", "700"],
+  variable: "--font-sans",
+});
+
+export const metadata = {
+  title: "Toby Gates | Senior Software Engineer",
+  description:
+    "Senior Software Engineer based in Bedfordshire, UK working on bespoke, fast, responsive and secure web applications.",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`font-sans ${inter.variable}`}>{children}</body>
+    </html>
+  );
+}
